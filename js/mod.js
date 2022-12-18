07775a94d9009910d923213f7a16aa700362ad9a
Glitch (umbrellaacadatree) <none>
2022-12-18 19:41:44 +0000

    First Export

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Umbrella Acadatree: Rewritten",
+	id: "tua",
+	author: "Five",
+	pointsName: "power",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "1.0",
+	name: "Back Again",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
